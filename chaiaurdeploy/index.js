c2b29fc83ed03875995ce70427c1a74256fced47
shiv-pencia <shivprakashpencia@gmail.com>
2024-05-21 15:06:33 +0530

    first commit

--- chaiaurdeploy/index.js
+++ chaiaurdeploy/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get('/twitter',(req,res)=>{
-    res.send('shiv.pencia98298')
-})
-app.get('/login',(req,res)=>{
-    res.send('<h1>Please Login at shiv.com</h1>')
-})
-app.get('/youtube',(req,res)=>{
-    res.send('<h2>youtube.com</h2>')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/twitter", (req, res) => {
+  res.send("shiv.pencia98298");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login at shiv.com</h1>");
+});
+app.get("/youtube", (req, res) => {
+  res.send("<h2>youtube.com</h2>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
